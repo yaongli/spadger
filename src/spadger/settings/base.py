728ed696b31861fbd595dddce77cdb6ac3f901c0
Yang Yongli <yongli.yang@dianping.com>
2016-02-17 18:29:42 +0800

    nginx

--- src/spadger/settings/base.py
+++ src/spadger/settings/base.py
@@ -58,7 +58,7 @@ if exists(env_file):
 # Raises ImproperlyConfigured exception if SECRET_KEY not in os.environ
 SECRET_KEY = env('SECRET_KEY')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -126,8 +126,6 @@ LOCALE_PATHS = (
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
-
-ALLOWED_HOSTS = []
 
 # Crispy Form Theme - Bootstrap 3
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
